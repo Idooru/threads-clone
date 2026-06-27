273c050cb1ff11507cb37d3e6092b5eefe602ea2
shlee <shere1765@gmail.com>
2026-06-27 13:22:55 +0900

    12. CSS와의 차이점, 비슷한 점

--- app/(tabs)/(home)/index.tsx
+++ app/(tabs)/(home)/index.tsx
@@ -1,49 +1,117 @@
+import { BlurView } from "expo-blur";
 import { usePathname, useRouter } from "expo-router";
-import { Text, TouchableOpacity, View } from "react-native";
+import {
+  Dimensions,
+  Image,
+  PixelRatio,
+  StyleSheet,
+  Text,
+  TouchableOpacity,
+  View,
+} from "react-native";
+import { useSafeAreaInsets } from "react-native-safe-area-context";
 
 export default function Index() {
   const router = useRouter();
   const pathname = usePathname();
+  const insets = useSafeAreaInsets();
+  const isLoggedIn = false;
 
   console.log("pathname", pathname);
+  console.log("insets", insets);
+
+  const { width, height } = Dimensions.get("window");
+
+  console.log(`화면 너비: ${width} 화면 높이: ${height}`);
+  console.log(
+    `화면 너비: ${width * PixelRatio.get()}px, 높이: ${height * PixelRatio.get()}px`,
+  );
 
   return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-    >
-      <View>
-        <TouchableOpacity onPress={() => router.navigate(`/`)}>
-          <Text style={{ color: pathname === "/" ? "red" : "black" }}>
-            For you
-          </Text>
-        </TouchableOpacity>
+    <>
+      <View
+        style={[
+          styles.container,
+          { paddingTop: insets.top, paddingBottom: insets.bottom },
+        ]}
+      >
+        <BlurView style={styles.header} intensity={70}>
+          <Image
+            style={styles.headerLogo}
+            source={require("../../../assets/images/react-logo.png")}
+          />
+          {!isLoggedIn && (
+            <TouchableOpacity
+              style={styles.loginButton}
+              onPress={() => router.navigate("/login")}
+            >
+              <Text style={styles.loginButtonText}>로그인</Text>
+            </TouchableOpacity>
+          )}
+        </BlurView>
+        {isLoggedIn && (
+          <View style={styles.tabContainer}>
+            <View style={styles.tab}>
+              <TouchableOpacity onPress={() => router.navigate(`/`)}>
+                <Text style={{ color: pathname === "/" ? "red" : "black" }}>
+                  For you
+                </Text>
+              </TouchableOpacity>
+            </View>
+            <View style={styles.tab}>
+              <TouchableOpacity onPress={() => router.navigate("/following")}>
+                <Text style={{ color: pathname === "/" ? "black" : "red" }}>
+                  Following
+                </Text>
+              </TouchableOpacity>
+            </View>
+          </View>
+        )}
+        <View>
+          <TouchableOpacity onPress={() => router.push("/@zerocho/post/1")}>
+            <Text>게시글1</Text>
+          </TouchableOpacity>
+        </View>
+        <View>
+          <TouchableOpacity onPress={() => router.push("/@zerocho/post/2")}>
+            <Text>게시글2</Text>
+          </TouchableOpacity>
+        </View>
+        <View>
+          <TouchableOpacity onPress={() => router.push("/@zerocho/post/3")}>
+            <Text>게시글3</Text>
+          </TouchableOpacity>
+        </View>
       </View>
-      <View>
-        <TouchableOpacity onPress={() => router.navigate("/following")}>
-          <Text style={{ color: pathname === "/" ? "black" : "red" }}>
-            Following
-          </Text>
-        </TouchableOpacity>
-      </View>
-      <View>
-        <TouchableOpacity onPress={() => router.push("/@zerocho/post/1")}>
-          <Text>게시글1</Text>
-        </TouchableOpacity>
-      </View>
-      <View>
-        <TouchableOpacity onPress={() => router.push("/@zerocho/post/2")}>
-          <Text>게시글2</Text>
-        </TouchableOpacity>
-      </View>
-      <View>
-        <TouchableOpacity onPress={() => router.push("/@zerocho/post/3")}>
-          <Text>게시글3</Text>
-        </TouchableOpacity>
-      </View>
-    </View>
+    </>
   );
 }
+
+const styles = StyleSheet.create({
+  container: { flex: 1 },
+  tabContainer: { flexDirection: "row" },
+  tab: {
+    flex: 1,
+    alignItems: "center",
+    paddingVertical: 15,
+  },
+  header: { alignItems: "center" },
+  headerLogo: {
+    width: 42, // DP, DIP
+    height: 42,
+  },
+  loginButton: {
+    position: "absolute",
+    right: 20,
+    top: 0,
+    borderWidth: 1,
+    borderColor: "black",
+    borderRadius: 10,
+    paddingVertical: 10,
+    paddingHorizontal: 20,
+    backgroundColor: "black",
+  },
+  loginButtonText: {
+    color: "white",
+  },
+});
